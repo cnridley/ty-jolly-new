110e83ae973273689f3091db94104b0b22499f03
cnridley <cnridley1@gmail.com>
2021-01-10 14:33:32 +0000

    deployment

--- tyjollynew/settings.py
+++ tyjollynew/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xdw$fosm)avk7h(kpjy1&-b(k-^i320a0iu*ksut*0$c$aua_!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEVELOPMENT in os.environ
 
 ALLOWED_HOSTS = ['ty-jolly-new.herokuapp.com', 'localhost']
 
